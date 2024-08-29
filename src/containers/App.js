2e4579bbd9132ba27c5940ff7d31e25d20830cdf
Nishiman <m.danish051003@gmail.com>
2024-08-29 19:40:50 +0800

    Adding a few changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox.js';
 import Scroll from '../components/Scroll.js';
+import ErrorBoundry from '../components/ErrorBoundry.js';
 import './App.css';
 
 
@@ -39,7 +40,9 @@ class App extends Component  {
                 <h1 className='f1'>Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                    <CardList robots={filteredRobots} />
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         )
